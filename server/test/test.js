0f291ec2060e39335706581964b1d431584163ee
Aphrodice Imanizabayo <34691089+Aphrodis@users.noreply.github.com>
2018-11-23 09:59:33 +0200

    Update test.js

--- server/test/test.js
+++ server/test/test.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require('./deliveries');
+const app = require('../routes/deliveries');
 const should = chai.should(); 
 
 chai.use(chaiHttp);
